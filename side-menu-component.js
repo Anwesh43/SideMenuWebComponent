0a6a46468181e6e0a8bfc09f23045e279f87ac30
Anwesh43 <anweshthecool0@gmail.com>
2017-06-25 19:04:57 +0530

    Adding HamburgIcon

--- side-menu-component.js
+++ side-menu-component.js
@@ -5,6 +5,9 @@ class SideMenuComponent extends HTMLElement  {
         this.img = document.createElement('img')
         this.ul = document.createElement('ul')
         this.childObj = []
+        this.img.style.position = 'absolute'
+        this.img.style.left = w/20
+        this.img.style.top = w/20
         for(var i=0;i<this.children.length;i++) {
             const child = this.children[i]
             this.childObj.push({text:child.text,href:child.href})
@@ -15,9 +18,52 @@ class SideMenuComponent extends HTMLElement  {
     }
     render() {
         const canvas = document.createElement('canvas')
-        canvas.width = w/10
-        canvas.height = h/10
+        canvas.width = w/20
+        canvas.height = w/20
         const context = canvas.getContext('2d')
         this.img.src = canvas.toDataURL()
+    }
+    update() {
+
+    }
+}
+class HamburgIcon {
+    constructor(img) {
+        this.ly = w/40
+        this.img = img
+        this.dir = 0
+    }
+    draw(context) {
+        context.strokeStyle = 'white'
+        context.lineWidth = w/100
+        context.save()
+        context.translate(w/40,w/40)
+        for(var i=0;i<3;i++) {
+            context.beginPath()
+            context.moveTo(-w/40,(i-1)*this.ly)
+            context.lineTo(w/40,(i-1)*this.ly))
+            context.stroke()
+        }
+        context.restore()
+    }
+    startUpdating(dir) {
+        this.dir = dir
+    }
+    update() {
+        this.img.style.left = parseFloat(this.img.style.left) + this.dir*(w/50);
+        this.ly -= this.dir*(w/200);
+        if(this.ly>w/40) {
+            this.dir = 0
+            this.ly = w/40
+            this.img.style.left = w/20
+        }
+        if(this.ly<0) {
+            this.ly = 0
+            this.img.style.left = 3*w/20
+            this.dir = 0
+        }
+    }
+    stopped() {
+        return this.dir == 0
     }
 }
